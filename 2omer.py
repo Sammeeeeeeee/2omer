666928b16e5798708d24a3a879624fa0b5e90ae0
Sammeeeeeeee <139072031+Sammeeeeeeee@users.noreply.github.com>
2024-03-05 01:16:18 +0000

    V2.0.0
    Major UI changes, codebase refactor.

--- 2omer.py
+++ 2omer.py
@@ -2,130 +2,139 @@ import sys
 import os
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QSpinBox, QSystemTrayIcon, QMenu, QGridLayout, QHBoxLayout, QSizePolicy
 from PyQt5.QtCore import QTimer, Qt
-from PyQt5.QtGui import QIcon
-from plyer import notification
+from PyQt5.QtGui import QIcon, QFont, QFontDatabase
+
+# Constants for application
+FONT_PATH = "C:/Windows/Fonts/segoeui.ttf"
+FONT_NAME = "Segoe UI Variable"
+FONT_SIZE = 14
+ICON_PATH = "clock.png"
+APP_TITLE = "2omer"
+APP_WIDTH = 450
+APP_HEIGHT = 250
+DEFAULT_FOCUS_MINS = 20
+DEFAULT_FOCUS_SECS = 0
+DEFAULT_BREAK_MINS = 0
+DEFAULT_BREAK_SECS = 20
+TIMER_INTERVAL = 1000
+SPINBOX_WIDTH = 60
+
 
 class TimerApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("2omer: 20:00")
-        self.setGeometry(100, 100, 450, 200)  # Adjusted window width
-        self.setFixedSize(450, 200)  # Adjusted fixed size
+        self.setWindowTitle(f"{APP_TITLE}: {DEFAULT_FOCUS_MINS:02}:{DEFAULT_FOCUS_SECS:02}")
+        self.setGeometry(100, 100, APP_WIDTH, APP_HEIGHT)
+        self.setFixedSize(APP_WIDTH, APP_HEIGHT)
+        self.load_font()
 
-        self.default_focus_minutes = 20
-        self.default_focus_seconds = 0
-        self.default_break_minutes = 0
-        self.default_break_seconds = 20
-
-        self.focus_minutes = self.default_focus_minutes
-        self.focus_seconds = self.default_focus_seconds
-        self.break_minutes = self.default_break_minutes
-        self.break_seconds = self.default_break_seconds
-
-        self.time_left = self.focus_minutes * 60 + self.focus_seconds
-        self.is_focus_period = True
+        self.init_period_values()
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_timer)
         self.timer.timeout.connect(self.update_tooltip)
 
         self.setup_ui()
+        self.add_system_tray_icon()
 
+    def init_period_values(self):
+        self.focus_minutes = DEFAULT_FOCUS_MINS
+        self.focus_seconds = DEFAULT_FOCUS_SECS
+        self.break_minutes = DEFAULT_BREAK_MINS
+        self.break_seconds = DEFAULT_BREAK_SECS
+        self.time_left = self.focus_minutes * 60 + self.focus_seconds
+        self.is_focus_period = True
+
+    def load_font(self):
+        QFontDatabase.addApplicationFont(FONT_PATH)
+        
     def setup_ui(self):
         layout = QVBoxLayout()
         self.setup_period_display(layout)
         self.setup_time_input(layout)
         self.setup_buttons(layout)
-
         self.setLayout(layout)
-        self.add_system_tray_icon()
 
     def setup_period_display(self, layout):
-        self.period_label = QLabel()
-        self.period_label.setAlignment(Qt.AlignCenter)  # Align label to center
+        self.period_label = self.create_label("", alignment=Qt.AlignCenter, font_name=FONT_NAME, font_size=FONT_SIZE)
         layout.addWidget(self.period_label)
 
     def setup_time_input(self, layout):
         grid_layout = QGridLayout()
-
-        # Adjusted labels position
-        grid_layout.addWidget(QLabel("Focus Period:"), 1, 0)
-        grid_layout.addWidget(QLabel("Break Period:"), 2, 0)
-
-        focus_period_layout = QHBoxLayout()
-        self.focus_minutes_spinbox = self.create_spinbox(self.focus_minutes, "", 0, 59)
-        focus_period_layout.addWidget(self.focus_minutes_spinbox)
-        focus_period_layout.addWidget(QLabel("minutes"))
-        self.focus_seconds_spinbox = self.create_spinbox(self.focus_seconds, "", 0, 59)
-        focus_period_layout.addWidget(self.focus_seconds_spinbox)
-        focus_period_layout.addWidget(QLabel("seconds"))
-        grid_layout.addLayout(focus_period_layout, 1, 1)
-
-        break_period_layout = QHBoxLayout()
-        self.break_minutes_spinbox = self.create_spinbox(self.break_minutes, "", 0, 59)
-        break_period_layout.addWidget(self.break_minutes_spinbox)
-        break_period_layout.addWidget(QLabel("minutes"))
-        self.break_seconds_spinbox = self.create_spinbox(self.break_seconds, "", 0, 59)
-        break_period_layout.addWidget(self.break_seconds_spinbox)
-        break_period_layout.addWidget(QLabel("seconds"))
-        grid_layout.addLayout(break_period_layout, 2, 1)
-
+        grid_layout.addWidget(self.create_label("Focus Period:"), 1, 0, Qt.AlignLeft)
+        grid_layout.addWidget(self.create_label("Break Period:"), 2, 0, Qt.AlignLeft)
+        self.focus_minutes_spinbox, self.focus_seconds_spinbox = self.create_period_layout(grid_layout, 1, self.focus_minutes, self.focus_seconds)
+        self.break_minutes_spinbox, self.break_seconds_spinbox = self.create_period_layout(grid_layout, 2, self.break_minutes, self.break_seconds)
         layout.addLayout(grid_layout)
 
-    def setup_buttons(self, layout):
-        self.start_button = QPushButton("Start", self)
-        self.start_button.clicked.connect(self.start_timer)
-        layout.addWidget(self.start_button)
+    def create_period_layout(self, layout, row, minutes, seconds):
+        period_layout = QHBoxLayout()
+        minutes_spinbox = self.create_spinbox(minutes, "", 0, 59)
+        period_layout.addWidget(minutes_spinbox)
+        period_layout.addWidget(self.create_label("minutes"), alignment=Qt.AlignLeft)
+        seconds_spinbox = self.create_spinbox(seconds, "", 0, 59)
+        layout.addLayout(period_layout, row, 1)
+        layout.addWidget(seconds_spinbox, row, 2, alignment=Qt.AlignRight)
+        layout.addWidget(self.create_label("seconds"), row, 3, alignment=Qt.AlignLeft)
+        return minutes_spinbox, seconds_spinbox
 
-        self.reset_button = QPushButton("Reset", self)
-        self.reset_button.clicked.connect(self.reset_timer)
+    def setup_buttons(self, layout):
+        self.start_button = self.create_button("Start", self.start_timer)
+        self.reset_button = self.create_button("Reset", self.reset_timer)
+        layout.addWidget(self.start_button)
         layout.addWidget(self.reset_button)
+
+    def create_button(self, title, function):
+        button = QPushButton(title, self)
+        button.clicked.connect(function)
+        return button
+
+    def create_label(self, text, alignment=None, font_name=None, font_size=None):
+        label = QLabel(text)
+        if alignment:
+            label.setAlignment(alignment)
+        if font_name and font_size:
+            label.setFont(QFont(font_name, font_size))
+        return label
 
     def create_spinbox(self, value, suffix, min_value, max_value):
         spinbox = QSpinBox(self)
         spinbox.setSuffix(suffix)
         spinbox.setRange(min_value, max_value)
         spinbox.setValue(value)
-        spinbox.valueChanged.connect(self.validate_input)
-        
-        # Set focus policy to Qt.StrongFocus
         spinbox.setFocusPolicy(Qt.StrongFocus)
-
-        # Set size policy to fixed and adjust width for better appearance
         spinbox.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        spinbox.setFixedWidth(80)  # Adjust width as needed
-
+        spinbox.setFixedWidth(SPINBOX_WIDTH)
+        spinbox.setStyleSheet("QSpinBox { padding: 5px; border: 1px solid #d4d4d4; border-radius: 5px; background-color: #f0f0f0; }")
+        spinbox.valueChanged.connect(self.validate_input)
         return spinbox
 
     def add_system_tray_icon(self):
         self.tray_icon = QSystemTrayIcon(self)
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        icon_path = os.path.join(script_dir, "clock.png")
-        try:
-            self.tray_icon.setIcon(QIcon(icon_path))
-        except FileNotFoundError as e:
-            print("Icon file not found:", e)
+        self.tray_icon.setIcon(QIcon(self.get_script_dir_path(ICON_PATH)))
         tray_menu = QMenu(self)
         exit_action = tray_menu.addAction("Exit")
         exit_action.triggered.connect(self.close)
         self.tray_icon.setContextMenu(tray_menu)
         self.update_tooltip()
         self.tray_icon.show()
-        app_icon = QIcon(icon_path)
-        self.setWindowIcon(app_icon)
+        self.setWindowIcon(QIcon(self.get_script_dir_path(ICON_PATH)))
+
+    def get_script_dir_path(self, filename):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        return os.path.join(script_dir, filename)
 
     def start_timer(self):
+        self.set_custom_times()  # Set custom times first
         if not self.timer.isActive():
-            self.set_custom_times()
-            self.timer.start(1000)
+            self.set_period_time()  # Update period time based on custom input
+            self.timer.start(TIMER_INTERVAL)
             self.start_button.setEnabled(False)
 
     def reset_timer(self):
         self.timer.stop()
-        self.time_left = self.focus_minutes * 60 + self.focus_seconds
-        self.is_focus_period = True
+        self.init_period_values()
         self.update_timer_display()
-        self.setWindowTitle("2omer: 20:00")
         self.start_button.setEnabled(True)
 
     def update_timer(self):
@@ -135,16 +144,14 @@ class TimerApp(QWidget):
             self.show_notification()
             self.switch_period()
             self.set_period_time()
-            self.timer.start(1000)
+            self.timer.start(TIMER_INTERVAL)
         else:
             self.update_timer_display()
 
     def update_timer_display(self):
-        minutes = self.time_left // 60
-        seconds = self.time_left % 60
-        time_str = f"{minutes:02}:{seconds:02}"
+        self.setWindowTitle(f"{APP_TITLE}: {self.format_time(self.time_left)}")
         period = "Focus Period" if self.is_focus_period else "Break Period"
-        self.period_label.setText(f"<b>{period}: {time_str}</b>")
+        self.period_label.setText(f"<b>{period}: {self.format_time(self.time_left)}</b>")
 
     def set_custom_times(self):
         self.focus_minutes = self.focus_minutes_spinbox.value()
@@ -154,6 +161,7 @@ class TimerApp(QWidget):
 
     def switch_period(self):
         self.is_focus_period = not self.is_focus_period
+        self.set_period_time()  # Update period time based on whether it's a focus or break period
 
     def set_period_time(self):
         if self.is_focus_period:
@@ -162,17 +170,10 @@ class TimerApp(QWidget):
             self.time_left = self.break_minutes * 60 + self.break_seconds
 
     def show_notification(self):
-        notification_title = "Timer Alert"
+        notification_title = "2omer"
         period = "break" if self.is_focus_period else "focus period"
         notification_text = f"It's time for a {period}"
-        try:
-            notification.notify(
-                title=notification_title,
-                message=notification_text,
-                app_name="2omer"
-            )
-        except Exception as e:
-            print("Failed to show notification:", e)
+        self.tray_icon.showMessage(notification_title, notification_text)
 
     def update_tooltip(self):
         self.tray_icon.setToolTip(self.format_time(self.time_left))
